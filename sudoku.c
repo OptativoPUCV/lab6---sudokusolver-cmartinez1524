b522d19dcebd9c0cc8b14a23be0b02ce7b387135
Your Name <you@example.com>
2024-05-17 02:15:32 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -125,7 +125,7 @@ Node* DFS(Node* initial, int* cont) {
             push(S, adjNode); // Insertar cada nodo adyacente en la pila
             adjNode = next(adjNodes);
         }
-        freeList(adjNodes); // Liberar la lista de nodos adyacentes
+        
         // No liberamos `current` ya que no se desea usar `free`
     }
 
